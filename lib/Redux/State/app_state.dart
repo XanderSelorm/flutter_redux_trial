73f407f145762ac4293207e856bd65e3f666384a
XanderSelorm <alexandercubby@gmail.com>
2021-06-14 23:24:30 +0000

    WIP: restructured the code some more 2

--- lib/Redux/State/app_state.dart
+++ lib/Redux/State/app_state.dart
@@ -1,13 +1,18 @@
-import 'package:flutter_redux_trial/Redux/State/root_state.dart';
-
 import '../../Models/todo_item.dart';
 
-class AppState extends RootState {
+class AppState {
+  final bool? isLoading;
+  final String? currentErrorMessage;
   final List<TodoItem> todoItems;
 
   AppState({
+    this.isLoading,
+    this.currentErrorMessage,
     required this.todoItems,
   });
 
-  AppState.initialState() : todoItems = List.unmodifiable(<TodoItem>[]);
+  AppState.initialState()
+      : todoItems = List.unmodifiable(<TodoItem>[]),
+        isLoading = false,
+        currentErrorMessage = "";
 }
--- lib/Redux/State/root_state.dart
+++
@@ -1,9 +0,0 @@
-class RootState {
-  final bool? isLoading;
-  final String? currentErrorMessage;
-
-  RootState({
-    this.isLoading,
-    this.currentErrorMessage,
-  });
-}
